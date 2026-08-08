69a3cadecdb62c9a689fc404183181351c7167a2
Daemontail <gurtejs169@gmail.com>
2026-08-08 22:39:27 +0530

    Added order rendering system

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import menuArray from "./data.js";
-function getMenuItems(arr){
+
+const orders = []
+document.addEventListener('click',function(e){
+    if(e.target.dataset.add){
+        return addOrderItem(e.target.dataset.add)
+    }
+})
+function getMenuHtml(arr){
     return arr.map(function(menuitem){
         const {name,ingredients,id,price,emoji} = menuitem
         return `
-        <div class="item-container" id="item-container-${id}">
+        <div class="item-container">
                 <div class="item-container-inner">
                 <h2 class="item">${emoji}</h2>
                 <div class="item-details">
@@ -12,10 +19,33 @@ function getMenuItems(arr){
                 <p class="price">$${price}</p>
                 </div>
                 </div>
-                <button class="add-item">+</button>
+                <button class="add-item" data-add='${id}'>+</button>
             </div>
 
         `
     })
 }
-document.getElementById('menu').innerHTML=getMenuItems(menuArray)
+
+function addOrderItem(item){
+    orders.push({name: menuArray[item].name,
+                price: menuArray[item].price})
+    renderOrder()
+}
+function renderOrder(){
+    renderOrderedItems()
+}
+function renderOrderedItems(){
+    const ordereditems = document.getElementById('orders')
+    ordereditems.innerHTML=orders.map(function(order){
+            return `
+            <div class="order">
+            <div class="order-name">
+            <h3>${order.name}</h3>
+            <button data-remove='${order.name}'>remove</button>
+            </div>
+            <h3>$${order.price}</h3>
+            </div>
+            `
+    }).join("")
+}
+document.getElementById('menu').innerHTML=getMenuHtml(menuArray)
